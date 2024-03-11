39e4e8ed6545531b2b52c900d50ef38df0a43e54
vinit agarwal <vinitbtech@gmail.com>
2024-03-11 04:42:14 +0000

    Developed stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select 
+-- from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordersellingprice,
+o.ordercostprice,
+--from raw customer
+c.customername,
+c.segment,
+c.country,
+--from raw prodcut
+p.category,
+p.productname,
+p.subcategory 
+from {{ ref('raw_orders') }} as O 
+LEFT join {{ ref('raw_customer') }} as C 
+ON O.customerid = c.customerid
+LEFT join {{ ref('raw_product') }} as P 
+ON O.productid = P.productid
