a43d52aec74aa950ec406251179bb129f1acedaf
vinit agarwal <vinitbtech@gmail.com>
2024-03-12 05:42:11 +0000

    added delivery_team seed and reffenced in stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select 
 -- from raw orders
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid','p.productid']) }} as SK_orders,
 o.orderid,
 o.orderdate,
 o.shipdate,
@@ -17,11 +18,12 @@ p.productid,
 p.category,
 p.productname,
 p.subcategory,
-{{ markup('ordersellingprice', 'ordercostprice' ) }}  as markup
+{{ markup('ordersellingprice', 'ordercostprice' ) }}  as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as O 
 LEFT join {{ ref('raw_customer') }} as C 
 ON O.customerid = c.customerid
 LEFT join {{ ref('raw_product') }} as P 
 ON O.productid = P.productid
-
-{{limit_data_in_dev('orderdate')}}
+Left join {{ ref('delivery_team') }} as d
+ON o.shipmode=d.shipmode
